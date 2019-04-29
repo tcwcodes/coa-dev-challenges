a4c28e7ffec2b82ced0344e8f1c0208eb9bb5d81
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2019-04-28 19:35:15 -0500

    commit to deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+# Austin Transportation Frontend Homework using React
+
+## An example using React and Socrata SODA API
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+### To get started:
+
+- Install dependencies: `npm install`
+- Run a local server: `npm start`
+- Find `TODO:`s in the code and build from there.
---
+++ build/precache-manifest.996eb764dc7f9061d62ba7428cbcfbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5a9cfa4d561691063a3",
+    "url": "/static/js/main.e1cdfe1b.chunk.js"
+  },
+  {
+    "revision": "e0cc60baeb002ac97423",
+    "url": "/static/js/2.76b2af99.chunk.js"
+  },
+  {
+    "revision": "b5a9cfa4d561691063a3",
+    "url": "/static/css/main.850a6fcf.chunk.css"
+  },
+  {
+    "revision": "e0cc60baeb002ac97423",
+    "url": "/static/css/2.5910007f.chunk.css"
+  },
+  {
+    "revision": "6d91f8d995870d001b0893574121677b",
+    "url": "/index.html"
+  }
+];
